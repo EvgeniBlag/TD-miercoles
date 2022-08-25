61fcda7e4071d08ccf8b311681493be79059882e
EvgeniBlag <blagodarni@gmail.com>
2022-08-25 12:56:04 +0200

    Updates

---
+++ precache-manifest.4d7abffd6f29addb5203a8bbc1e4b6c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d99bbd0c55bbc18063a142d9702e1af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60651b15a99992c65ca9",
+    "url": "/static/js/2.e8eed9ba.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e8eed9ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa72eaa5b7f0d58ba40f",
+    "url": "/static/js/main.187efd99.chunk.js"
+  },
+  {
+    "revision": "5a60fc9df39256935c27",
+    "url": "/static/js/runtime-main.1e2b0c03.js"
+  }
+]);
