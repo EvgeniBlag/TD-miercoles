7e6991b5ff5fa42b5490cfebcfe47f7fe7f769bd
EvgeniBlag <blagodarni@gmail.com>
2022-09-01 08:39:28 +0200

    test2

--- src/state/tasks-reduser.test.tsx
+++ src/state/tasks-reduser.test.tsx
@@ -1,5 +1,5 @@
 import { TasksStateType } from "../App"
-import { removeTaskAC, tasksReducer } from "./tasks-reduser";
+import { removeTaskAC, tasksReducer,addTaskAC, changeStatusAC, changeTaskTitleAC } from "./tasks-reduser";
 
 test('correct task shuld de deleted from correct array', ()=>{
     const startState : TasksStateType = { 
@@ -32,3 +32,87 @@ expect(endState).toEqual(
         ]   
    });
 });
+
+
+test('correct task should be added to correct array', () => {
+   const startState: TasksStateType = {
+       "todolistId1": [
+           { id: "1", title: "CSS", isDone: false },
+           { id: "2", title: "JS", isDone: true },
+           { id: "3", title: "React", isDone: false }
+       ],
+       "todolistId2": [
+           { id: "1", title: "bread", isDone: false },
+           { id: "2", title: "milk", isDone: true },
+           { id: "3", title: "tea", isDone: false }
+       ]
+   };
+
+   const action = addTaskAC("juce", "todolistId2");
+
+   const endState = tasksReducer(startState, action)
+
+   expect(endState["todolistId1"].length).toBe(3);
+   expect(endState["todolistId2"].length).toBe(4);
+   expect(endState["todolistId2"][0].id).toBeDefined();
+   expect(endState["todolistId2"][0].title).toBe('juce');
+   expect(endState["todolistId2"][0].isDone).toBe(false);
+})
+
+
+test('correct task change status', () => {
+   const startState: TasksStateType = {
+       "todolistId1": [
+           { id: "1", title: "CSS", isDone: false },
+           { id: "2", title: "JS", isDone: true },
+           { id: "3", title: "React", isDone: false }
+       ],
+       "todolistId2": [
+           { id: "1", title: "bread", isDone: false },
+           { id: "2", title: "milk", isDone: true },
+           { id: "3", title: "tea", isDone: false }
+       ]
+   };
+
+
+   const action = changeStatusAC("3", true,"todolistId2");
+
+   const endState = tasksReducer(startState, action)
+
+   expect(endState["todolistId1"].length).toBe(3);
+   expect(endState["todolistId2"].length).toBe(3);
+   expect(endState["todolistId2"][0].id).toBeDefined();
+   expect(endState["todolistId2"][2].title).toBe('tea');
+   expect(endState["todolistId2"][2].isDone).toBe(true);
+})
+
+test('correct task change title', () => {
+    const startState: TasksStateType = {
+        "todolistId1": [
+            { id: "1", title: "CSS", isDone: false },
+            { id: "2", title: "JS", isDone: true },
+            { id: "3", title: "React", isDone: false }
+        ],
+        "todolistId2": [
+            { id: "1", title: "bread", isDone: false },
+            { id: "2", title: "milk", isDone: true },
+            { id: "3", title: "tea", isDone: false }
+        ]
+    };
+ 
+ 
+    const action = changeTaskTitleAC ("3", 'EvgenyBlagodarnyy',"todolistId2");
+ 
+    const endState = tasksReducer(startState, action)
+ 
+    expect(endState["todolistId1"].length).toBe(3);
+    expect(endState["todolistId2"].length).toBe(3);
+    expect(endState["todolistId2"][0].id).toBeDefined();
+    expect(endState["todolistId2"][2].title).toBe('EvgenyBlagodarnyy');
+    expect(endState["todolistId2"][2].isDone).toBe(false);
+ })
+
+
+
+
+
